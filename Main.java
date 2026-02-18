e952ed043a4b4d4510fdeb567637d553dd7fa5cc
usamaktkdev <usamaktk.dev@gmail.com>
2026-02-19 00:15:17 +0500

    Add files

--- Main.java
+++ Main.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
 
 public class Main{
+    public static int  addNumbers(int a, int b) {
+        return a + b;
+    };
     public static void main(String[] args) {
           Scanner scanner = new Scanner(System.in);
-         for (int i = 5; i >=1; i--) {
-             for (int j = 1; j <= 5-i; j++) {
-
-                 System.out.print(" ");
-             }
-             for (int k = 1; k <=i; k++) {
-
-                 System.out.print(k+" ");
-             }
-             System.out.println();
-         }
+         
+          int sum = addNumbers(1, 2);
+          System.out.println(sum);
         
     }
 }
---
+++ nested_pattern.java
@@ -0,0 +1,23 @@
+// import java.util.Scanner;
+
+// public class Main{
+//     public static void main(String[] args) {
+//           Scanner scanner = new Scanner(System.in);
+//          for (int i = 5; i >=1; i--) {
+//              for (int j = 1; j <= 5-i; j++) {
+
+//                  System.out.print(" ");
+//              }
+//              for (int k = 1; k <=i; k++) {
+
+//                  System.out.print(k+" ");
+//              }
+//              System.out.println();
+//          }
+        
+//     }
+// }
+
+
+
+
